5110b0a5d6b852eb235102398b059fda289375e1
Thehumo <93725833+Thehumo@users.noreply.github.com>
2022-10-28 22:11:12 +0200

    dbconnection file changes

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => ('DB_HOST'),
+            
+            'host' => env('DB_HOST'),
             'port' => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD$', ''),
+            'password'  => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'MYSQL_ATTR_SSL_CA' => env('MYSQL_ATTR_SSL_CA'),
             'charset' => 'utf8mb4',
